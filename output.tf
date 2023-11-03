558f81810454999a420c95bcdf3f74a9ff11b70d
Sven Schliesing <s.schliesing@tagesschau.de>
2023-11-03 12:25:46 +0100

    add viewer

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ output "WORKLOAD_IDENTITY_POOL_PROVIDER_NAME" {
   value = module.wif.workload_identity_pool_provider_name
 }
 
-output "GCP_SA" {
+output "GCP_SERVICE_ACCOUNT" {
   value = google_service_account.gitlab_ci.email
+}
+
+output "GCP_SERVICE_ACCOUNT_VIEWER" {
+  value = google_service_account.gitlab_ci_viewer.email
 }
--- wif.tf
+++ wif.tf
@@ -13,6 +13,21 @@ resource "google_project_iam_member" "gitlab_ci" {
   ])
 }
 
+resource "google_service_account" "gitlab_ci_viewer" {
+  project    = module.showcase.project_id
+  account_id = "gitlab-ci-viewer"
+}
+
+resource "google_project_iam_member" "gitlab_ci_viewer" {
+  project = module.showcase.project_id
+  member  = "serviceAccount:${google_service_account.gitlab_ci_viewer.email}"
+  role    = each.key
+
+  for_each = toset([
+    "roles/viewer"
+  ])
+}
+
 module "wif" {
   source            = "gitlab.com/gitlab-com/gcp-oidc/google" # https://gitlab.com/gitlab-com/gl-security/security-operations/infrastructure-security-public/oidc-modules/-/tree/main/terraform-modules/gcp-oidc?ref_type=heads
   version           = "3.1.1"
@@ -22,6 +37,10 @@ module "wif" {
   oidc_service_account = {
     "owner" = {
       sa_email  = google_service_account.gitlab_ci.email
+      attribute = "attribute.ref/main"
+    }
+    "viewer" = {
+      sa_email  = google_service_account.gitlab_ci_viewer.email
       attribute = "attribute.project_id/${var.gitlab_project_id}"
     }
   }
